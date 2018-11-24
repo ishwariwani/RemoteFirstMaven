e6b25e14e643b617249e0532b79357d100e1e112
Ishwari <ishwariwani@gmail.com>
2018-11-24 16:44:28 -0600

    My first commit

---
+++ FirstMaven/src/main/java/Testing/NewTest.java
@@ -0,0 +1,32 @@
+package Testing;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+public class NewTest {
+	public WebDriver driver;
+	
+  @Test
+  public void f() {
+	  driver.get("https://www.softwaretestingmaterial.com/selenium-tutorial/");
+	  
+  }
+  @BeforeClass
+  public void beforeClass() {
+	  System.setProperty("webdriver.chrome.driver", "C:\\SeleniumDrivers\\chromedriver.exe");
+	   driver = new ChromeDriver();
+	   
+	   System.out.println("This is my first project");
+	 
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  driver.quit();
+  }
+
+}
